d38e34ef194b303913a687d67834bb1db4ba1b89
benwasden <was24001@byui.edu>
2024-05-02 21:13:51 -0700

    Spelling correction

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,26 +1,22 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
+const li = document.createElement('li');
+const deleteButton = document.createElement('button');
 
 button.addEventListener('click', function() {
     if (input.value.trim() !== '') {
-
-        // Create li element and delete button
-        const li = document.createElement('li');
-        const deleteButton = document.createElement('button');
-
         li.textContent = input.value;
         deleteButton.textContent = '❌';
-        li.appendChild(deleteButton);
-        list.appendChild(li)
-        input.value = "";
-        input.focus();
-        deleteButton.addEventListeniner('click', () => {
-            list.removeChild(li);
-            input.focus();
-        });
-    }
-    else {
-        input.focus();
+        li.append(deleteButton);
+        list.append(li);
     }
 });
+
+deleteButton.addEventListener('click', function() {
+    list.removeChild(li);
+    input.focus();
+});
+
+input.value = '';
+input.focus();
